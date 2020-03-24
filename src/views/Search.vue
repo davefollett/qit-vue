fb70e831b753240dbea85816da64640f248ba5fa
Dave Follett <davefollett@gmail.com>
2020-03-24 14:13:00 -0400

    added axios and connected the search text field to make an api call.  The results can be inspected in Chrome's Vue dev tools.

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -14,13 +14,16 @@
 <script>
 import SearchBar from '@/components/SearchBar.vue'
 
+const axios = require('axios')
+
 export default {
   name: 'Home',
   components: {
     SearchBar
   },
   data: () => ({
-    query: 'default'
+    query: 'default',
+    searchResults: {}
   }),
   beforeRouteUpdate (to, from, next) {
     this.query = to.params.query
@@ -34,6 +37,19 @@ export default {
   methods: {
     search: function () {
       console.log(`Q: ${this.query}`)
+      const token = Buffer.from(`${process.env.VUE_APP_ELASTIC_USERNAME}:${process.env.VUE_APP_ELASTIC_PASSWORD}`, 'utf8').toString('base64')
+      const URL = `${process.env.VUE_APP_CORS_WRAPPER}/${process.env.VUE_APP_BASE_ELASTIC_URL}`
+        .replace('{searchTerm}', this.query)
+        .replace('{maxResults}', process.env.VUE_APP_MAX_RESULTS)
+      axios
+        .get(URL, {
+          headers: {
+            Authorization: `Basic ${token}`
+          }
+        })
+        .then(response => {
+          this.searchResults = response.data
+        })
     }
   }
 }
