904ebf3029d7166aae586e56dc25e05b24e701a8
Dave Follett <davefollett@gmail.com>
2020-03-29 18:13:49 -0400

    switched the latest 10 to use a carousel

---
+++ src/components/SearchResultsCarousel.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card
+    raised
+  >
+    <v-card-title>{{ title }}</v-card-title>
+    <hr>
+    <v-carousel
+      height="200"
+      show-arrows-on-hover
+      cycle
+      :dark="$vuetify.theme.dark"
+      :light="!$vuetify.theme.dark"
+    >
+      <v-carousel-item
+        v-for="result in results"
+        :key="result._id"
+      >
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class="overline mb-4">{{result._source.published}}</div>
+            <v-list-item-title class="subtitle-2 mb-1 text-wrap">{{ result._source.episode_title }}</v-list-item-title>
+            <v-list-item-subtitle>{{result._source.podcast_title}}</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar
+            tile
+          >
+            <v-icon x-large color="teal">mdi-playlist-plus</v-icon>
+          </v-list-item-avatar>
+        </v-list-item>
+        <!-- <audio :src="audioUrl" controls preload="none"></audio> -->
+      </v-carousel-item>
+    </v-carousel>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchResultsCarousel',
+  props: {
+    results: {
+      type: Object,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,47 +3,23 @@
     <v-container>
       <v-row justify="center">
         <p class="title">
-          Welcome to QIT, the website that searches for software develper podcasts by topic.
+          Welcome to QIT, the website that searches for software developer podcasts by topic.
         </p>
       </v-row>
 
       <v-row justify="center">
         <p>
-          Get started searching or checkout the lastest 10 podcasts.
+          Get started searching the {{ totalIndexed }} podcasts indexed or checkout the lastest 10.
         </p>
       </v-row>
-    </v-container>
 
-    <v-row justify="center">
-      <SearchBar></SearchBar>
-    </v-row>
-
-    <v-container>
       <v-row justify="center">
-        <p>Total Podcasts Indexed: {{ totalIndexed }}</p>
+        <SearchBar></SearchBar>
       </v-row>
 
-      <div v-if="lastTen">
-        <hr class="mb-4">
-        <p>Most Recent 10 Podcasts</p>
-        <SearchResultCard
-          class="mb-4"
-          v-for="result in lastTen"
-          :key="result._id"
-          :result="result"/>
-      </div>
-      <div v-else>
-        <v-container fluid>
-          <v-row justify="center">
-            <v-progress-circular
-              indeterminate
-              color="teal"
-              size="80"
-              width="8"
-            ></v-progress-circular>
-          </v-row>
-        </v-container>
-      </div>
+      <SearchResultsCarousel
+        title="10 Most Recent Podcasts"
+        :results="lastTen"/>
 
     </v-container>
   </div>
@@ -51,13 +27,13 @@
 
 <script>
 import SearchBar from '@/components/SearchBar.vue'
-import SearchResultCard from '@/components/SearchResultCard.vue'
+import SearchResultsCarousel from '@/components/SearchResultsCarousel.vue'
 
 export default {
   name: 'Home',
   components: {
     SearchBar,
-    SearchResultCard
+    SearchResultsCarousel
   },
   mounted () {
     this.$store.dispatch('SearchResults/totalIndexed')
